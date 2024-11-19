15e4ca780fafa66897d93bc611fd8ed0e97f8939
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-11-19 21:27:25 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "path_to_your_file.csv";
+        List<string> countries = new List<string>();
+
+        try
+        {
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(',');
+
+                string country = fields[5].Trim('"');
+
+                if (!countries.Contains(country))
+                {
+                    countries.Add(country);
+                }
+            }
+            Console.WriteLine("Danh sách các quốc gia:");
+            foreach (string country in countries)
+            {
+                Console.WriteLine(country);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Đã xảy ra lỗi: " + ex.Message);
+        }
+    }
+}
